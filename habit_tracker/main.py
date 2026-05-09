873a7a346a405b280af162e799266bda8e6ef50c
EABene <benedikt.sandler@gmail.com>
2026-05-09 15:14:33 +0200

    Completion of app

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 import json
 from datetime import date, timedelta
@@ -12,7 +11,7 @@ FILE_PATH = Path(__file__).parent / "habits.json"
 # Funktion um Habit hinzuzufügen
 def add_habit(habit_list, habit):
     if habit not in habit_list:
-        data[habit] = []
+        habit_list[habit] = []
 
 # Funktion um Habit abzuhaken
 def complete_habit(habit_list, habit):
@@ -30,8 +29,17 @@ def streak_calc(habit_list):
 
 # Funktion, um alle Habits auszugeben und ihren Streak
 def show_habits(habit_list):
+    print(25 * "-")
     for habit in habit_list:
         print(f"{habit} - Streak: {streak_calc(habit_list[habit])} Days")
+    print(25 * "-")
+
+#Funktion um eine Habit zu Löschen
+def delete_habit(habit_list, habit):
+    if habit in habit_list:
+        del habit_list[habit]
+        print(habit, "deleted.")
+    else: print("habit not found in list.")
 
 
 # initialisiert JSON File
@@ -41,21 +49,45 @@ if not FILE_PATH.exists():
 
 # Lesen der JSON
 with open(FILE_PATH, "r") as f:
-    data = json.load(f)
+    habit_list = json.load(f)
 
 # Eigentliche App-Ausführung
-add_habit(data, "Tooth brushing")
-add_habit(data, "Sports")
-add_habit(data, "Meditate")
-complete_habit(data, "Sports")
-complete_habit(data, "Meditate")
-show_habits(data)
+user_input = ""
+while user_input != "5":
 
-# Schreibt alles in der Variable Data in JSON File
+    user_input = input("""Habit Tracker App Menu:
+    1 - Habit hinzufügen
+    2 - Habit erledigt markieren
+    3 - Alle Habits anzeigen
+    4 - Habit löschen
+    5 - Beenden
+    Was würdest du gerne tun? >> """)
+
+    possible_options = ["1", "2", "3", "4", "5"]
+    if user_input not in possible_options:
+        print("Invalid input.")
+
+    function_keys = {
+        "1": add_habit,
+        "2": complete_habit,
+        "3": show_habits,
+        "4": delete_habit,
+        "5": "quit"
+    }
+
+    if user_input in ["1", "2", "4"]:
+        habit = input("Welche Habit möchtest du bearbeiten? >> ")
+        function_keys[user_input](habit_list, habit)
+    elif user_input == "3":
+        function_keys[user_input](habit_list)
+    elif user_input == "5":
+        print("Programm wird beendet.")
+    
+
+
+# Schreibt alles in der Variable habit_list in JSON File
 with open(FILE_PATH, "w") as f:
-    json.dump(data, f, indent=2)
-
-
+    json.dump(habit_list, f, indent=2)
 
 
 print("done")
