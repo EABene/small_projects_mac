87ac37d556aff6951e2633f2285f6d220a06b088
EABene <benedikt.sandler@gmail.com>
2026-05-13 18:20:34 +0200

    Work on main

--- expense_tracker/main.py
+++ expense_tracker/main.py
@@ -1,4 +1,8 @@
 import sqlite3
+import datetime 
+
+today = str(datetime.date.today())
+print(today)
 
 # Verbindung öffnen (erstellt .db Datei falls nicht existiert)
 conn = sqlite3.connect("expenses.db")
@@ -6,11 +10,64 @@ conn = sqlite3.connect("expenses.db")
 # Cursor — das ist dein "Werkzeug" um Befehle zu schicken
 cursor = conn.cursor()
 
-# Befehl ausführen
-cursor.execute("SQL-BEFEHL HIER")
+# Tabelle erstellen if not exists
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS expenses (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT,
+        amount REAL,
+        category TEXT,
+        date TEXT
+    )
+""")
 
-# Änderungen speichern
-conn.commit()
+
+def view_expenses():
+    pass
+
+def add_expense():
+    description = input("Title of expense: >> ")
+    amount = float(input("Amount of expense: >> "))
+    category = input("Category of expense: >> ")
+
+    # Tabelleneintrag hinzufügen
+    cursor.execute("""
+        INSERT INTO expenses (description, amount, category, date)
+        VALUES (?, ?, ?, ?)
+    """, (description, amount, category, today))
+
+    # Änderungen speichern
+    conn.commit()
+
+def del_expense():
+    pass
+
+def sum_category():
+    pass
+
+
+
+# Eigentliches Programm
+
+user_input = input("""-----Expense Tracker-----
+1: Ausgaben anzeigen
+2: Ausgabe hinzufügen
+3: Ausgabe löschen
+-------------------------
+>>> """)
+
+keys = {
+    '1': view_expenses,
+    '2': add_expense,
+    '3': del_expense,
+    '4': sum_category
+
+}
+
+if user_input in ['1', '2', '3', '4']:
+    keys[user_input]()
+else: print("Invalid input.")
+
 
 # Verbindung schließen
 conn.close()
