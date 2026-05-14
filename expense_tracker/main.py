dc683289d59c20b80051c37aeedd6088a7c31151
EABene <benedikt.sandler@gmail.com>
2026-05-14 20:40:09 +0200

    Keep working...

--- expense_tracker/main.py
+++ expense_tracker/main.py
@@ -23,9 +23,16 @@ cursor.execute("""
 
 
 def view_expenses():
-    pass
+    # Just print table
+    print("ID   |Expense             |Amount    |Category       |Date")
+    cursor.execute("SELECT * FROM expenses")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(f"{row[0]:<5} {row[1]:<20} {row[2]:<10} {row[3]:<15} {row[4]}")
+
 
 def add_expense():
+    # User Input erfragen
     description = input("Title of expense: >> ")
     amount = float(input("Amount of expense: >> "))
     category = input("Category of expense: >> ")
@@ -40,7 +47,18 @@ def add_expense():
     conn.commit()
 
 def del_expense():
-    pass
+    # User input erfragen
+    id = input("ID of expense to delete: >> ")
+
+    # Tabelleneintrag löschen
+    cursor.execute("""
+        DELETE FROM expenses WHERE id = ?
+    """, (id,))
+
+    #Änderungen speichern
+    conn.commit()
+
+
 
 def sum_category():
     pass
@@ -48,25 +66,35 @@ def sum_category():
 
 
 # Eigentliches Programm
-
-user_input = input("""-----Expense Tracker-----
-1: Ausgaben anzeigen
-2: Ausgabe hinzufügen
-3: Ausgabe löschen
--------------------------
->>> """)
-
 keys = {
     '1': view_expenses,
     '2': add_expense,
     '3': del_expense,
     '4': sum_category
-
 }
 
-if user_input in ['1', '2', '3', '4']:
-    keys[user_input]()
-else: print("Invalid input.")
+user_input = ""
+
+while user_input != '5':
+    user_input = input("""-----Expense Tracker-----
+1: Ausgaben anzeigen
+2: Ausgabe hinzufügen
+3: Ausgabe löschen
+4: Kategorie zusammenfassen
+5: Programm BEENDEN
+-------------------------
+>>> """)
+    if user_input in ['1', '2', '3', '4']:
+        keys[user_input]()
+    elif user_input == '5':
+        break
+    else: print("Invalid input.")
+
+print("Programm erfolgreich beendet.")
+
+
+
+
 
 
 # Verbindung schließen
