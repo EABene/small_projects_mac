46dd0b684a84488ec2aaca57523caff3fe89c6b5
EABene <benedikt.sandler@gmail.com>
2026-05-22 17:58:37 +0200

    Start up new project

---
+++ bank_account_manager/main.py
@@ -0,0 +1,28 @@
+
+class BankAccount:
+    def __init__(self, name, account_number, password, balance):
+        self.name = name
+        self.account_number = account_number
+        self.password = password
+        self.balance = balance
+
+
+konto = BankAccount("John", "1234", "geheim", "1000€")
+print(konto.balance)
+print(konto.name)
+
+
+
+def greet(name, account_number, balance):
+
+    print(f"""Welcome, {name}!
+    Account Number: {account_number}
+    Balance: €{balance}
+
+    1 - Einzahlen
+    2 - Abheben
+    3 - Kontostand anzeigen
+    4 - Transaktionsverlauf
+    5 - Sparkonto anzeigen""")
+
+# greet("Ben")
